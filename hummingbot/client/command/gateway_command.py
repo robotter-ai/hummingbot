07c2868bd9b0a6c88449c0345bc33566c6783e08
Danilo Araújo Silva <danilo.silva@funttastic.com>
2025-04-17 19:49:26 -0300

    Reverting client changes to have a clean branch when compared with HB

--- hummingbot/client/command/gateway_command.py
+++ hummingbot/client/command/gateway_command.py
@@ -287,10 +287,8 @@ class GatewayCommand(GatewayChainApiManager):
                             balances: Dict[str, Any] = await self._get_gateway_instance().get_balances(
                                 chain, network, w, [native_token]
                             )
-                            balance = (
-                                balances.get(native_token)
-                                or balances.get("balances", {}).get(native_token)
-                                or balances.get("balances", {}).get("total", {}).get(native_token)
+                            balance = balances["balances"].get(native_token) or balances["balances"]["total"].get(
+                                native_token
                             )
                             wallet_table.append({"balance": balance, "address": w})
 
@@ -323,11 +321,8 @@ class GatewayCommand(GatewayChainApiManager):
                         )
                         wallet_table: List[Dict[str, Any]] = [
                             {
-                                "balance": (
-                                    balances.get(native_token)
-                                    or balances.get("balances", {}).get(native_token)
-                                    or balances.get("balances", {}).get("total", {}).get(native_token)
-                                ),
+                                "balance": balances["balances"].get(native_token)
+                                or balances["balances"]["total"].get(native_token),
                                 "address": wallet_address,
                             }
                         ]
